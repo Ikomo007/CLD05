df7804353950476525aac2f984c9f2fb59943770
Ikomo007 <livingrock.iyke@gmail.com>
2023-11-08 18:46:13 -0500

    first commit

---
+++ vpc_module/output.tf
@@ -0,0 +1,3 @@
+output "vpc-name" {
+  value = aws_vpc.vpc[0].bucket
+}
---
+++ vpc_module/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "vpc_cidr" {
+  default = "172.0.0.0/24"
+}
+variable "subnets_cidr" {
+  type    = list(string)
+  default = ["172.0.0.0/25", "172.0.0.128/25"]
+}
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
